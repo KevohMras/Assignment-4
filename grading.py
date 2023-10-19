0f8b1610a47411d368351bffd99e3d2246cb19f7
KevohMras <84986640+KevohMras@users.noreply.github.com>
2023-10-19 07:42:15 +0300

    first commit

---
+++ grading.py
@@ -0,0 +1,29 @@
+def calculate_grade(score, grading_scale):
+    for grade, min_score in grading_scale.items():
+        if score >= min_score:
+            return grade
+    return "F"  # Default to "F" if the score is below the minimum value in the grading scale.
+
+def main():
+    grading_scale = {
+        "A": 90,
+        "B": 80,
+        "C": 70,
+        "D": 60,
+    }
+
+    num_students = int(input("Enter the number of students: "))
+    student_data = {}
+
+    for i in range(1, num_students + 1):
+        student_name = input(f"Enter the name of student {i}: ")
+        student_mark = float(input(f"Enter the mark for {student_name}: "))
+        student_data[student_name] = student_mark
+
+    print("\nGrades:")
+    for student, mark in student_data.items():
+        grade = calculate_grade(mark, grading_scale)
+        print(f"{student}: {mark} - {grade}")
+
+if __name__ == "__main__":
+    main()
